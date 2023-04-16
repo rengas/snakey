2b52b02264692035379ebe221daae8b05697502b
rengas <mr.rengaswamy@gmail.com>
2023-04-17 07:02:27 +0800

    Remove unused errros

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -5,35 +5,3 @@ type ValidationError string
 func (e ValidationError) Error() string {
 	return string(e)
 }
-
-const ErrInternalError InternalError = "Sorry! Something is broken"
-
-type InternalError string
-
-func (e InternalError) Error() string {
-	return string(e)
-}
-
-const ErrUnAuthorizedError UnAuthorizedError = "not authorised"
-
-type UnAuthorizedError string
-
-func (e UnAuthorizedError) Error() string {
-	return string(e)
-}
-
-const ErrForbidden ForbiddenErr = "invalid credentials"
-
-type ForbiddenErr string
-
-func (e ForbiddenErr) Error() string {
-	return string(e)
-}
-
-const ErrNotFound NotFoundError = "not found"
-
-type NotFoundError string
-
-func (e NotFoundError) Error() string {
-	return string(e)
-}
